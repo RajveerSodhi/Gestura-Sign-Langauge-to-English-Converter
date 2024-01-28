858ff8b2f33a87a0217adb9f5e25d15c48d4b1a8
RajveerSodhi <rajveersodhi03@gmail.com>
2024-01-28 08:22:43 -0800

    aliens fix

--- animate_alien.py
+++ animate_alien.py
@@ -1,18 +1,19 @@
 from moviepy.editor import *
 
-clip = [
-    "default to thumb out",
-    "default to fingers out",
-    "fingers out to default",
-    "fingers out to thumb out",
-    "thumb out to default",
-    "thumb out to fingers out",
-    "fingers out to fingers out",
-    "thumb out to thumb out"
-]
+def animate_alien(text):
+    morse = encrypt(text)
 
+    clip = [
+        "default to thumb out",
+        "default to fingers out",
+        "fingers out to default",
+        "fingers out to thumb out",
+        "thumb out to default",
+        "thumb out to fingers out",
+        "fingers out to fingers out",
+        "thumb out to thumb out"
+    ]
 
-def animate_alien(morse):
     morse = morse.strip()
     result = []
 
@@ -34,7 +35,6 @@ def animate_alien(morse):
             result.append("default to thumb out")
         elif morse[i - 1] == " " and morse[i] == ".":
             result.append("default to fingers out")
-    result.append("fingers out to default" if morse[-1] == "." else "thumb out to default")
 
     final_videos = []
     for clip in result:
@@ -43,5 +43,12 @@ def animate_alien(morse):
     final_video = concatenate_videoclips(final_videos, method="compose")
     final_video.write_videofile("alien_morse.mp4", verbose=False, logger=None, threads=64, fps=20)
 
+    # loading video dsa gfg intro video
+    clip = VideoFileClip("alien_morse.mp4")
 
-animate_alien("--. --... . _  ")
+    clip = clip.subclip(0)
+
+    # previewing the clip at fps = 20
+    clip.preview(fps=20)
+
+animate_alien("--. --... .  ")
