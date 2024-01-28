d728b7dde93c2e171e921ab98a52ff2db1e94f1f
RajveerSodhi <rajveersodhi03@gmail.com>
2024-01-28 08:31:43 -0800

    alien gives output

--- animate_alien.py
+++ animate_alien.py
@@ -1,7 +1,40 @@
 from moviepy.editor import *
 
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+
+            # Looks up the dictionary and adds the
+            # corresponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+    return cipher
+
 def animate_alien(text):
-    morse = encrypt(text)
+    morse = encrypt(text.upper())
 
     clip = [
         "default to thumb out",
@@ -51,4 +84,4 @@ def animate_alien(text):
     # previewing the clip at fps = 20
     clip.preview(fps=20)
 
-animate_alien("--. --... .  ")
+animate_alien("ribhav")
