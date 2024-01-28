462bc9e7e8f169719f8ec89acc2c014d0b0783c4
Sanjith Senthil <109128028+SanjithSenthil@users.noreply.github.com>
2024-01-27 17:57:38 -0800

    Update language.py

--- language.py
+++ language.py
@@ -1,21 +1,25 @@
 import googletrans
 # 3.1.0a0
 from googletrans import Translator
-text = '''How are you doing'''
 
-translator = Translator()
+def change_language(text, lang):
+    translator = Translator()
+    out = translator.translate(text, dest=lang)
+    return out.text
 
-# French
-out = translator.translate(text, dest="fr")
-print(out.text)
-# Spanish
-out = translator.translate(text, dest="es")
-print(out.text)
-# Chinese
-out = translator.translate(text, dest="zh-CN")
-print(out.text)
-# Japanese
-out = translator.translate(text, dest="ja")
-print(out.text)
-
-
+# text = '''How are you doing'''
+#
+# translator = Translator()
+#
+# # French
+# out = translator.translate(text, dest="fr")
+# print(out.text)
+# # Spanish
+# out = translator.translate(text, dest="es")
+# print(out.text)
+# # Chinese
+# out = translator.translate(text, dest="zh-CN")
+# print(out.text)
+# # Japanese
+# out = translator.translate(text, dest="ja")
+# print(out.text)
